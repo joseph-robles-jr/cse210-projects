6e8afe31b17014139bb25c1773829a96da6c2896
Joseph Robles Jr. <156927934+joseph-robles-jr@users.noreply.github.com>
2024-09-25 20:40:39 -0600

    Update Program.cs

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Collections.Concurrent;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
+
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+           
+        int intNum = -100;
+        while (intNum != magicNumber) 
+        {
+            Console.Write("What is the magic number?: ");
+            string readNumber = Console.ReadLine();
+            intNum = int.Parse(readNumber);
+            
+            if (magicNumber > intNum)
+            {
+                Console.WriteLine("Higher!");
+            }
+            else if (magicNumber < intNum)
+            {
+                Console.WriteLine("Lower!");
+            }
+        }
+
+        if (intNum == magicNumber) 
+        {
+            Console.WriteLine("Congrats! That is the right number!");
+        }  
+
+
+    }  
 }
