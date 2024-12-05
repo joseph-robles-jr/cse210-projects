582b7e1b9eec2994bbbb199a6528ea4b89af89ce
joseph-robles-jr <rjosepha@byui.edu>
2024-12-05 07:49:38 -0700

    set up initial program structure. Made progress on the QSO class

---
+++ final/FinalProject/Qso.cs
@@ -0,0 +1,46 @@
+using System.Dynamic;
+using System.Runtime.CompilerServices;
+
+abstract class Qso 
+{
+   protected float _frequency;
+   protected int _rstRx;
+   protected int _rstTx;
+   protected string _state; // 2 digit state abrv. (i.e. TX, AZ, ID)
+   protected string _callsign; //callsign of rx station
+
+   protected string _date; //set with getDateTime()
+
+    protected abstract void setRx();
+    protected abstract void setTx();
+   
+    protected void setFreq() //get the frequency and assign it to _frequesncy
+    {
+        Console.Write("Enter the frequency (i.e. 28.445): ");
+        _frequency = float.Parse(Console.ReadLine());   
+    }
+
+    protected void setState() 
+    {
+        Console.Write("Enter the State of the Recived Station (i.e. AZ) : ");
+        _state = Console.ReadLine();
+    }
+
+    protected void setCallsign()
+    {
+        Console.Write("Callsign of Recived station (i.e. K7BYI) : ");
+        _callsign = Console.ReadLine();
+    }
+    
+    protected void setTime() //returns the date and time for the log
+    {
+    DateTime now = DateTime.UtcNow; // Format the DateTime object to the desired format 
+    _date = now.ToString("dd, MMMM, yyyy - HH:mm:ss"); // Output the formatted date
+    }
+ 
+
+
+
+
+
+}
