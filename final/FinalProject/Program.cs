4602673ff4a28136ce6b1f84243755f91d131449
joseph-robles-jr <rjosepha@byui.edu>
2024-12-08 13:24:34 -0700

    added new QSOarrayclass

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -1,23 +1,49 @@
 using System;
 
-//This Program requires you to use "dotnet build" and "dotnet run"
-
 class Program
 {
-
-    static void Main()
+    static void Main(string[] args)
     {
-        Console.WriteLine("Creating QSO...");
+        QSOarray qsoArray = new QSOarray();
+        bool exit = false;
 
-        // Create an instance of the Qso class
-        USB USB = new USB();
+        while (!exit)
+        {
+            Console.WriteLine("\nQSO Menu:");
+            Console.WriteLine("1. Create USB QSO");
+            Console.WriteLine("2. Create LSB QSO");
+            Console.WriteLine("3. Create CW QSO");
+            Console.WriteLine("4. Create AM QSO");
+            Console.WriteLine("5. Create FM QSO");
+            Console.WriteLine("6. Exit");
+            Console.Write("Select an option: ");
+            
+            string choice = Console.ReadLine();
 
-        // Set the details for the QSO
-        USB.newQso();
-        
-
-        // List the details of the QSO
-        USB.listQso();
+            switch (choice)
+            {
+                case "1":
+                    qsoArray.newUsbQso();
+                    break;
+                case "2":
+                    qsoArray.newLsbQso();
+                    break;
+                case "3":
+                    qsoArray.newCwQso();
+                    break;
+                case "4":
+                    qsoArray.newAmQso();
+                    break;
+                case "5":
+                    qsoArray.newFmQso();
+                    break;
+                case "6":
+                    exit = true;
+                    break;
+                default:
+                    Console.WriteLine("Invalid option. Please try again.");
+                    break;
+            }
+        }
     }
 }
-
--- final/FinalProject/QSOarray.cs
+++ final/FinalProject/QSOarray.cs
@@ -1,4 +1,49 @@
 public class QSOarray
 {
+    //make a list to hold the QSO types
+    List<object> _qsoLists = new List<object>();
+
+    
+
+// This function creates a new QSO object of a specified type and adds it to a list.
+private void createNewQSO<T>() where T : new() // The function is generic, allowing any type T that has a parameterless constructor.
+    {
+        // Create a new instance of the specified type T.
+        T qso = new T();
+
+        // Add the new instance to the _qsoLists list.
+        _qsoLists.Add(qso);
+
+        // Cast the new instance to dynamic to allow calling its methods without knowing the exact type at compile time.
+        dynamic dynamicQSO = qso;
+
+        // Call the newQso method on the new instance.
+        dynamicQSO.newQso();
+    }
+
+public void newUsbQso()
+    {
+        createNewQSO<USB>();    
+    }
+
+public void newLsbQso()
+    {
+        createNewQSO<LSB>();    
+    }
+
+public void newCwQso()
+    {
+        createNewQSO<Cw>();    
+    }
+
+public void newAmQso()
+    {
+        createNewQSO<AM>();    
+    }
+
+public void newFmQso()
+    {
+        createNewQSO<FM>();    
+    }
 
 }
